b2514afc310c539cd565906812295119cb98672f
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2023-02-07 14:56:25 -0500

    fix comments in in LinkedList

--- src/utilities/LinkedList.java
+++ src/utilities/LinkedList.java
@@ -6,24 +6,35 @@ public class LinkedList<T> {
 	private Node _tail;
 	private int _size;
 	
-	// each node has data in it and points to the node before it
+	/**
+	 * each node has data in it and points to the node before it
+	 * @author Josh Berger
+	 */
 	private class Node {
 		private T _data;
 		private Node _next;
 		
-		// default constructor creates a node with nothing in it and pointing to nothing
+		/**
+		  * default constructor creates a node with nothing in it and pointing to nothing
+		  */
 		public Node() {
 			this(null, null);
 		}
 		
-		// constructor creates a node from the passed in values
+		/**
+		 * constructor creates a node from the passed in values
+		 * @param data
+		 * @param next
+		 */
 		public Node(T data, Node next) {
 			_data = data;
 			_next = next;
 		}
 	}
 	
-	// default constructor creates a list with only the head pointing to the tail and a size of 0
+	/**
+	 * default constructor creates a list with only the head pointing to the tail and a size of 0
+	 */
 	public LinkedList() {
 		_head = new Node(null, null);
 		_tail = new Node(null, null);
@@ -31,23 +42,34 @@ public class LinkedList<T> {
 		_size = 0;
 	}
 	
-	// checks if there are no nodes in between the sentinel nodes
+	/**
+	 * checks if there are no nodes in between the sentinel nodes
+	 * @return whether the LinkedList is empty
+	 */
 	public boolean isEmpty() {
 		return _head._next == _tail;
 	}
 	
-	// reverts to the head pointing to the tail and the size to 0
+	/**
+	 * reverts to the head pointing to the tail and the size to 0
+	 */
 	public void clear() {
 		_head._next = _tail;
 		_size = 0;
 	}
 	
-	// returns the size 
+	/**
+	 * returns the size
+	 * @return size of LinkedList
+	 */
 	public int size() {
 		return _size;
 	}
 	
-	// if the passed in element is valid adds it as a node to the front of the list adding 1 to the size
+	/**
+	 * if the passed in element is valid adds it as a node to the front of the list adding 1 to the size
+	 * @param element
+	 */
 	public void addToFront(T element) {
 		if (element == null) return;
 		Node node = new Node(element, _head._next);
@@ -55,12 +77,20 @@ public class LinkedList<T> {
 		_size++;
 	}
 	
-	// checks if the passed in target is in the list
+	/**
+	 * checks if the passed in target is in the list
+	 * @param target
+	 * @return whether the target is in the LinkedList
+	 */
 	public boolean contains(T target) {
 		return getIndex(target) > -1;
 	}
 	
-	// finds the index of the target and if not, returns -1
+	/**
+	 * finds the index of the target and if not, returns -1
+	 * @param target
+	 * @return the index of the target or -1 if not found
+	 */
 	private int getIndex(T target) {
 		if (target == null) return -1;
 		return getIndex(target, _head._next, 0);
@@ -72,7 +102,11 @@ public class LinkedList<T> {
 		return getIndex(target, node._next, index+1);
 	}
 	
-	// returns the matching node in the list to the passed in target
+	/**
+	 * returns the matching node in the list to the passed in target
+	 * @param target
+	 * @return node equivalent to target from LinkedList
+	 */
 	private Node findNode(T target) {
 		if (target == null) return null;
 		return findNode(target, _head._next);
@@ -84,7 +118,11 @@ public class LinkedList<T> {
 		return findNode(target, node._next);
 	}
 	
-	// returns the node before the one that was passed in
+	/**
+	 * returns the node before the one that was passed in
+	 * @param target
+	 * @return the prior node to target
+	 */
 	private Node previous(T target) {
 		if (target == null) return null;
 		return previous(target, _head);
@@ -96,7 +134,11 @@ public class LinkedList<T> {
 		return previous(target, node._next);
 	}
 	
-	// removes the passed in target node and decreases the size by 1 
+	/**
+	 * removes the passed in target node and decreases the size by 1
+	 * @param target
+	 * @return whether the target was removed
+	 */
 	public boolean remove(T target) {
 		if (target != null) {
 			Node p = previous(target);
@@ -107,7 +149,11 @@ public class LinkedList<T> {
 		return false;
 	}
 	
-	// returns the node before the tail
+	// 
+	/**
+	 * returns the node before the tail
+	 * @return the node at the last index
+	 */
 	private Node last() {
 		if (_head._next == _tail) return _head;
 		return last(_head._next);
@@ -117,8 +163,11 @@ public class LinkedList<T> {
 		if (n._next == _tail) return n;
 		return (last(n._next));
 	}
-	 
-	// adds an element as a node before the tail
+	
+	/**
+	 * adds an element as a node before the tail
+	 * @param element
+	 */
 	public void addToBack(T element) {
 		if (element == null) return;
 		Node node = new Node(element, _tail);
@@ -126,7 +175,9 @@ public class LinkedList<T> {
 		_size++;
 	}
 	
-	// makes the node's data into a string
+	/**
+	 * makes the node's data into a string
+	 */
 	public String toString() {
 		return toString("", _head._next);
 	}
@@ -139,7 +190,9 @@ public class LinkedList<T> {
 		return toString(string, node._next);
 	}
 	
-	// reverses the list
+	/**
+	 * reverses the list
+	 */
 	public void reverse() {
 		if (_head._next == _tail || size() == 1) return;
 		reverse(_head._next, _tail, last());
