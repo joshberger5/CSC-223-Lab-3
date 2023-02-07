4746b8ff547391c2dd644c10e12ccb48b71aaf65
joshberger5 <joshberger5@yahoo.com>
2023-02-07 12:53:52 -0500

    added LinkedEquivalenceClass and test

---
+++ src/utilities/LinkedEquivalenceClass.java
@@ -0,0 +1,61 @@
+package utilities;
+
+import java.util.Comparator;
+
+public class LinkedEquivalenceClass<T> {
+	T _canonical;
+	Comparator<T> _comparator;
+	LinkedList<T> _rest;
+	
+	public LinkedEquivalenceClass(Comparator<T> comparator) {
+		
+	}
+	
+	public T canonical() {
+		
+	}
+	
+	public boolean isEmpty() {
+		
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public void clearNonCanonical() {
+		
+	}
+	
+	public int size() {
+		
+	}
+	
+	public boolean add(T element) {
+		
+	}
+	
+	public boolean contains(T target) {
+		
+	}
+	
+	public boolean belongs(T target) {
+		
+	}
+	
+	public boolean remove(T target) {
+		
+	}
+	
+	public boolean removeCanonical() {
+		
+	}
+	
+	public boolean demoteAndSetCanonical(T element) {
+		
+	}
+	
+	public String toString() {
+		
+	}
+}
